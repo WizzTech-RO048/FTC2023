be04c79af839c4940b565bd572913204c1ebc5aa
VladTomici14 <vladtomici@gmail.com>
2022-10-25 22:34:49 +0300

    Added telemetry to the class and added the Slider class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -11,21 +11,24 @@ import java.util.concurrent.ScheduledExecutorService;
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class Robot {
 
+    public Telemetry telemetry;
+
     public Wheels wheels;
     public Gripper gripper;
+    public Slider slider;
 
     public Robot(final HardwareMap hardwareMap, final Telemetry t, ScheduledExecutorService scheduler) {
         telemetry = t;
 
         // --------- initializing the imu sensor --------
-        BNO055IMU imu_sensor = hardwareMap.get(BNO055IMU.class, "imu_sensor");
-        imu_sensor.initialize(new BNO055IMU.Parameters());
+        // BNO055IMU imu_sensor = hardwareMap.get(BNO055IMU.class, "imu_sensor");
+        // imu_sensor.initialize(new BNO055IMU.Parameters());
 
         // ----- parsing the parameters for initializing the Wheels class ----
         Wheels.Parameters wheels_parameters = new Wheels.Parameters();
         wheels_parameters.hardwareMap = hardwareMap;
         wheels_parameters.telemetry = telemetry;
-        wheels_parameters.imu_sensor = imu_sensor;
+        // wheels_parameters.imu_sensor = imu_sensor;
         wheels_parameters.scheduler = scheduler;
         wheels_parameters.rpm = 435;
         wheels_parameters.encoder_resolution = 384.5;
@@ -38,10 +41,14 @@ public class Robot {
         gripper = new Gripper(gripper_parameters);
 
         // TODO: add the slider
+        Slider.Parameters slider_parameters = new Slider.Parameters();
+        slider_parameters.armRaisedPosition = 5200; // 5200 is the maximum
+        slider_parameters.telemetry = telemetry;
+        slider_parameters.hardwareMap = hardwareMap;
+        slider_parameters.scheduler = scheduler;
+        slider = new Slider(slider_parameters);
     }
 
-
-    private final Telemetry telemetry;
 
     public void stopRobot() {
         wheels.stopEngines();
