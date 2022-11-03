2e089009745a9514f67e48db8f05650ccc513bb2
VladTomici14 <vladtomici@gmail.com>
2022-11-03 16:55:29 +0200

    Added a speed limit and a way to control the slider variously using the triggers.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -17,9 +17,10 @@ public class MainTeleOp extends OpMode {
     private Robot robot;
     private Controller controller1;
 
-    private boolean turbo = false;
-    private double slider_position;
-    public double RAISE_POWER = 0.5;
+    private double speed_limit;
+    private int k = 0;
+    private double raise_percentage;
+    public double RAISE_POWER = 1.0;
     private ScheduledFuture<?> lastSliderRaised;
 
     @Override
@@ -27,8 +28,12 @@ public class MainTeleOp extends OpMode {
         robot = new Robot(hardwareMap, telemetry, Executors.newScheduledThreadPool(1));
         controller1 = new Controller(gamepad1);
 
+        // ---------- raising the slider a little bit ------------
+//        lastSliderRaised = robot.slider.raise(Slider.Position.BASE, RAISE_POWER);
+
         // --------- initializing the robot --------
         robot.gripper.release();
+
     }
 
     // ------ the emergency stop function ---------
@@ -39,37 +44,55 @@ public class MainTeleOp extends OpMode {
         controller1.update();
 
         // -------- controlling the robot movement ------
-        double x = controller1.left_stick_x;
-        double y = controller1.left_stick_y;
-        double r = controller1.right_stick_x;
+        if (controller1.leftBumper()) {
+            speed_limit = 0.85;
+        } else {
+            speed_limit = 0.5;
+        }
+        double x = controller1.left_stick_x * speed_limit;
+        double y = -controller1.left_stick_y * speed_limit;
+        double r = -controller1.right_stick_x * speed_limit;
         robot.wheels.move(x, y, r);
+
+        // TODO: apply a button to enable/disable headless moving
+        // TODO: watch out for the differences between buttons dualshock 4
 
         // ------- printing the slider position --------
         robot.slider.getCurrentPositionSlider();
 
         // ------- controlling the gripper -------
-        if (controller1.rightBumper()) {
-            robot.gripper.grab();
-        } else if (controller1.leftBumper()) {
+        if (controller1.dpadRightOnce()) {
             robot.gripper.release();
+        } else if (controller1.dpadLeftOnce()) {
+            robot.gripper.grab();
         }
 
-        // ------- controlling the slider --------
+        // ------- controlling the slider on predefined positions -----
         if(!Utils.isDone(lastSliderRaised)) {
             return ;
         }
-        if (controller1.AOnce()) {
-            slider_position = 0.3;
+        if (controller1.YOnce()) {
+            raise_percentage = 1.0;
         } else if (controller1.BOnce()) {
-            slider_position = 0.6;
-        } else if (controller1.YOnce()) {
-            slider_position = 0.9;
+            raise_percentage = 0.7;
         } else if (controller1.XOnce()) {
-            slider_position = 0.0;
+            raise_percentage = 0.02;
+        } else if (controller1.AOnce()) {
+            raise_percentage = 0.0;
         } else {
             return ;
         }
-        lastSliderRaised = robot.slider.raiseArm(slider_position, RAISE_POWER);
+
+        // -------- controlling the arm --------
+        if (raise_percentage <= 0.9) {
+            raise_percentage += (double) (controller1.right_trigger / 10);
+        } else if (raise_percentage >= 0.1) {
+            raise_percentage -= (double) (controller1.left_trigger / 10);
+        }
+        lastSliderRaised = robot.slider.raise(raise_percentage, RAISE_POWER);
+
+        // ------- printing out the position of the slider --------
+        telemetry.addData("Slider position", raise_percentage);
     }
 
 }
