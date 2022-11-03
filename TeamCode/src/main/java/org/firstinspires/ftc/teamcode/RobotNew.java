bc0874a07f25e4e768eef8016e095097df30a6de
Pavel Prodan <prodanpavel05@gmail.com>
2022-11-03 18:02:54 +0200

    arcade and headless tested and working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotNew.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class RobotNew {
+    private final HardwareMap hardwareMap;
+    private final Telemetry telemetry;
+
+    private final DcMotor lf, lr, rf, rr;
+
+    private final BNO055IMU imu;
+
+    private double headingOffset = 0.0;
+    private Orientation angles;
+    private Acceleration gravity;
+
+    public RobotNew(final HardwareMap _hardwareMap, final Telemetry _telemetry) {
+        hardwareMap = _hardwareMap;
+        telemetry = _telemetry;
+
+        lf = hardwareMap.dcMotor.get("left_front");
+        lr = hardwareMap.dcMotor.get("left_rear");
+        rf = hardwareMap.dcMotor.get("right_front");
+        rr = hardwareMap.dcMotor.get("right_rear");
+
+
+        lf.setDirection(DcMotor.Direction.REVERSE);
+        lr.setDirection(DcMotor.Direction.REVERSE);
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu.initialize(parameters);
+    }
+
+    private void setMotorMode(DcMotor.RunMode mode, DcMotor... motors) {
+        for (DcMotor motor : motors) {
+            motor.setMode(mode);
+        }
+    }
+
+    public void runUsingEncoders() {
+        setMotorMode(DcMotor.RunMode.RUN_USING_ENCODER, lf, lr, rf, rr);
+    }
+
+    public void runWithZeroPowerBehavior() {
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void runWithoutEncoders() {
+        setMotorMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER, lf, lr, rf, rr);
+    }
+
+    public boolean isGyroCalibrated() {
+        return imu.isGyroCalibrated();
+    }
+
+    public void loop() {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
+        gravity = imu.getGravity();
+    }
+
+    private double getRawHeading() {
+        return angles.firstAngle;
+    }
+
+    public double getHeading() {
+        return (getRawHeading() - headingOffset) % (2.0 * Math.PI);
+    }
+
+    public double getHeadingDegrees() { return Math.toDegrees(getHeading()); }
+
+    public void resetHeading() {
+        headingOffset = getRawHeading();
+    }
+
+    private static double maxAbs(double... xs) {
+        double ret = Double.MIN_VALUE;
+        for (double x : xs) {
+            if (Math.abs(x) > ret) {
+                ret = Math.abs(x);
+            }
+        }
+        return ret;
+    }
+
+    public void setMotors(double _lf, double _lr, double _rf, double _rr) {
+        final double scale = maxAbs(1.0, _lf, _lr, _rf, _rr);
+        lf.setPower(_lf / scale);
+        lr.setPower(_lr / scale);
+        rf.setPower(_rf / scale);
+        rr.setPower(_rr / scale);
+    }
+    }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTwo_Alfa.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "TeleOpNew")
+public class TeleOpTwo_Alfa extends OpMode {
+    private RobotNew robot;
+    private ControllerNew controller;
+    private boolean arcadeMode = false;
+
+    @Override
+    public void init() {
+        robot = new RobotNew(hardwareMap, telemetry);
+        robot.runUsingEncoders();
+        robot.runWithZeroPowerBehavior();
+        controller = new ControllerNew(gamepad1);
+    }
+
+    @Override
+    public void init_loop() {
+        controller.update();
+        if (controller.AOnce()) {
+            arcadeMode = ! arcadeMode;
+        }
+        telemetry.addData("Gyro Ready?", robot.isGyroCalibrated() ? "YES" : "no.");
+        telemetry.addData("Arcade Mode (a)", arcadeMode ? "YES" : "no.");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        controller.update();
+        robot.loop();
+
+        if (controller.XOnce()) {
+            robot.resetHeading();
+        }
+        if (controller.AOnce()) {
+            arcadeMode = !arcadeMode;
+        }
+        telemetry.addData("Arcade Mode (a)", arcadeMode ? "YES" : "no.");
+        telemetry.addData("Heading (reset: x)", robot.getHeadingDegrees());
+        telemetry.update();
+
+        final double x = Math.pow(controller.left_stick_x, 3.0);
+        final double y = -Math.pow(controller.left_stick_y, 3.0);
+
+        final double rotation = Math.pow(controller.right_stick_x, 3.0);
+        final double direction = Math.atan2(x, y) + (arcadeMode ? robot.getHeading() : 0.0);
+        final double speed = Math.min(1.0, Math.sqrt(x * x + y * y));
+
+        final double lf = speed * Math.sin(direction + Math.PI / 4.0) + rotation;
+        final double rf = speed * Math.cos(direction + Math.PI / 4.0) - rotation;
+        final double lr = speed * Math.cos(direction + Math.PI / 4.0) + rotation;
+        final double rr = speed * Math.sin(direction + Math.PI / 4.0) - rotation;
+
+        robot.setMotors(lf, lr, rf, rr);
+    }
+}
