24f281669c437a352100b6edae84beb3f5fd3dd7
VladTomici14 <vladtomici@gmail.com>
2022-11-28 15:42:45 +0200

    Implemented new movement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Wheels.java
@@ -1,17 +1,22 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import kotlin.text.CharDirectionality;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import com.qualcomm.hardware.*;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +26,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Stream;
 
 
+// TODO: add the new robot movement code
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class Wheels {
     private static final List<String> MOTORS_NAMES = Arrays.asList(
@@ -31,122 +37,112 @@ public class Wheels {
     );
 
     private List<DcMotorEx> engines = new ArrayList<>();
-    private final double encoderTicksPerSecond;
+    public DcMotor lf, lr, rf, rr;
 
-
-    // private final BNO055IMU imu_sensor;
     private final Telemetry telemetry;
     private final ScheduledExecutorService scheduler;
     private final HardwareMap map;
 
-    // TODO: find the right configuration of the directions of the engines
-    private static DcMotorEx getEngine(HardwareMap map, String name) {
-        /*
-        * Initializing an individual engine by hardware mapping the name, setting the direction and the
-        * running mode.
-        *   @param map HardwareMap for reading the engine from the driver station configuration
-        *   @param name The name of the motor in the driver station configuration
-        *
-        *   @returns motor: The engine, as a DcMotorEx type of variable
-        * */
-
-        DcMotorEx motor = map.get(DcMotorEx.class, name);
-        if (name.contains("left")) {
-            motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        } else {
-            motor.setDirection(DcMotorSimple.Direction.FORWARD);
-        }
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        return motor;
-    }
+    // ------- imu variables ---------
+    private final Imu imu_sensor;
 
     Wheels(@NonNull final Parameters parameters) {
         telemetry = Objects.requireNonNull(parameters.telemetry, "Telemetry object was not set");
-        // imu_sensor = Objects.requireNonNull(parameters.imu_sensor, "IMU sensor was not set");
         scheduler = Objects.requireNonNull(parameters.scheduler, "Scheduler was not set");
         map = Objects.requireNonNull(parameters.hardwareMap, "HardwareMap was not passed");
 
+        Imu.Parameters imu_parameters = new Imu.Parameters();
+        imu_parameters.map = map;
+        imu_parameters.telemetry = telemetry;
+        imu_sensor = new Imu(imu_parameters);
+        imu_sensor.loop();
+
         // --------- setting up the engines individually -----
-        for (String motor_name : MOTORS_NAMES) {
-            engines.add(getEngine(map, motor_name));
-        }
+        lf = setEngine(map, true, true, "lf");
+        lr = setEngine(map, true, true,"lr");
+        rf = setEngine(map, true, true, "rf");
+        rr = setEngine(map, true, true, "rr");
 
-        if (parameters.encoder_resolution != 0 && parameters.rpm != 0) {
-            encoderTicksPerSecond = (parameters.rpm / 60) * parameters.encoder_resolution;
-            useEncoders(true);
-        } else {
-            encoderTicksPerSecond = 0;
-            useEncoders(false);
-        }
-
-        useBrakes(true);
     }
 
-    private void useEncoders(boolean condition) {
-        DcMotor.RunMode mode = condition ? DcMotor.RunMode.RUN_USING_ENCODER : DcMotor.RunMode.RUN_WITHOUT_ENCODER;
-        for(DcMotor engine : engines) {
-            engine.setMode(mode);
-        }
-    }
-
-    private void useBrakes(boolean condition) {
+    private static DcMotor setEngine(HardwareMap map,
+                                       boolean useZeroPowerBehaviorBraking,
+                                       boolean useEncoders,
+                                       String name) {
         /*
-        * A function that sets the ZeroPowerBehavior of an engine to braking when the engine power is 0.
-        * This is why a motor will stay still when it is not used.
-        * */
-        DcMotor.ZeroPowerBehavior behavior = condition ? DcMotor.ZeroPowerBehavior.BRAKE : DcMotor.ZeroPowerBehavior.FLOAT;
-        for(DcMotor engine : engines) {
-            engine.setZeroPowerBehavior(behavior);
-        }
-    }
-
-    public void move(double x, double y, double r) {
-        // ------ normalizing the values from the joysticks --------
-        x = Math.max(-1, Math.min(x, 1));
-        y = Math.max(-1, Math.min(y, 1));
-        r = Math.max(-1, Math.min(r, 1));
-
-        double[] input = {
-                x + y + r, // left front
-                y - x + r, // left rear
-                y - x - r, // left rear
-                y + x - r, // right rear
-        };
-
-        double highest = Arrays.stream(input).map(Math::abs).reduce(1, Math::max);
-
-        // -------- giving motors the power for the movement ------
-        for(int i = 0; i < input.length; i++) {
-            setPower(engines.get(i), input[i] / highest);
-        }
-    }
-
-    private void setPower(DcMotorEx engine, double power) {
-        /*
-         * Setting up the power in the engines
-         *  @param engine The engine that we want to use
-         *  @param power A value between [0.0, 1.0] that represents the power that we want the engine to move
+         * Initializing an individual engine by hardware mapping the name, setting the direction, the
+         * running mode, the condition of using encoders and enabling ZeroPowerBehavior
+         *   @param map HardwareMap for reading the engine from the driver station configuration
+         *   @param name The name of the motor in the driver station configuration
+         *   @param useEncoder The condition if we want to use the encoder for the engine
+         *   @param useZeroPowerBehaviorBraking The condition if we want to use the brakes
+         *
+         *   @returns motor: The engine, as a DcMotorEx type of variable
          * */
-        // TODO: what happens if the power parameter is a negative value
 
-        if (engine.getMode() == DcMotor.RunMode.RUN_USING_ENCODER) {
-            engine.setVelocity(Math.round(power * encoderTicksPerSecond));
-        } else {
-            engine.setPower(power);
+        // -------- reading the engine from the HardwareMap --------
+        DcMotor engine = map.get(DcMotor.class, name);
+
+        // --------- setting the direction of the engine ---------
+        engine.setDirection(name.contains("left") ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE);
+
+        // ------- setting the encoders for the engine ------
+        engine.setMode(useEncoders ? DcMotor.RunMode.RUN_USING_ENCODER : DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // -------- enabling the usage of braking when the engine is idle -------
+        engine.setZeroPowerBehavior(useZeroPowerBehaviorBraking ? DcMotor.ZeroPowerBehavior.BRAKE : DcMotor.ZeroPowerBehavior.FLOAT);
+
+        return engine;
+    }
+
+    public void setMotors(double x, double y, double rotation, boolean useArcadeMode) {
+        x = Math.pow(x, 3.0);
+        y = Math.pow(y, 3.0);
+        rotation = Math.pow(rotation, 3.0);
+
+        final double direction = Math.atan2(x, y) + (useArcadeMode ? 0.0 : imu_sensor.getHeading());
+        final double speed = Math.min(1.0, Math.sqrt(x * x + y * y));
+
+        final double lf = speed * Math.sin(direction + Math.PI / 4.0) + rotation;
+        final double rf = speed * Math.cos(direction + Math.PI / 4.0) - rotation;
+        final double lr = speed * Math.cos(direction + Math.PI / 4.0) + rotation;
+        final double rr = speed * Math.sin(direction + Math.PI / 4.0) - rotation;
+
+        setMotors(lf, lr, rf, rr);
+    }
+
+    public void setMotors(double _lf, double _lr, double _rf, double _rr) {
+        final double scale = maxAbs(1.0, _lf, _lr, _rf, _rr);
+        lf.setPower(_lf / scale);
+        lr.setPower(_lr / scale);
+        rf.setPower(_rf / scale);
+        rr.setPower(_rr / scale);
+    }
+
+    private static double maxAbs(double... xs) {
+        double ret = Double.MIN_VALUE;
+        for (double x : xs) {
+            if (Math.abs(x) > ret) {
+                ret = Math.abs(x);
+            }
         }
+        return ret;
     }
 
     public void stopEngines() {
-        engines.forEach(engine -> setPower(engine, 0.0));
+        for (DcMotorEx engine : engines) {
+            engine.setPower(0.0);
+        }
     }
-
 
     public static class Parameters {
         public HardwareMap hardwareMap = null;
         public Telemetry telemetry = null;
-        // public BNO055IMU imu_sensor = null;
+
         public ScheduledExecutorService scheduler = null;
-        public double encoder_resolution = 0.0;
+        public boolean use_encoders = true;
+        public boolean useZeroPowerBehaviorBraking = true;
+
         public double rpm = 0;
     }
 }
