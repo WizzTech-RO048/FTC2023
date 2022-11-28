6e6f830df07cbaa30c243fc4a61e54988a6bed38
VladTomici14 <vladtomici@gmail.com>
2022-11-28 15:49:06 +0200

    Added telemetry.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Gripper.java
@@ -8,14 +8,13 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import java.util.Objects;
 
 public class Gripper {
-
-    private double RELEASE_POSITION = 0.0;
-    private double GRAB_POSITION = 1.0;
+    private final double RELEASE_POSITION = 0.0;
+    private final double GRAB_POSITION = 1.0;
 
     private final Telemetry telemetry;
     private final HardwareMap hardwareMap;
 
-    private Servo gripper;
+    private final Servo gripper;
 
     Gripper(@NonNull final Parameters parameters) {
         telemetry = Objects.requireNonNull(parameters.telemetry, "Telemetry object was not set");
@@ -26,12 +25,10 @@ public class Gripper {
 
     public void grab() {
         gripper.setPosition(GRAB_POSITION);
-        telemetry.addLine("Grabbed the cone");
     }
 
     public void release() {
         gripper.setPosition(RELEASE_POSITION);
-        telemetry.addLine("Released the gripper");
     }
 
     public static class Parameters {
