b9a60474b3309e7bef488d4e9065555ccc8ed8f0
VladTomici14 <vladtomici@gmail.com>
2022-12-05 12:58:44 +0200

    Started to develop the scenarios for the autonomous.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomy/Scenarios.java
@@ -0,0 +1,46 @@
+//package org.firstinspires.ftc.teamcode.Autonomy;
+//
+//import org.firstinspires.ftc.robotcore.external.Telemetry;
+//
+//public class Scenarios {
+//
+//    public Scenarios(int scenario, Telemetry telemetry) {
+//
+//        switch(scenario) {
+//            case 1:
+//                // RED 1
+//
+//            case 2:
+//                // RED 2
+//
+//            case 3:
+//                // BLUE 1
+//
+//            case 4:
+//                // BLUE 2
+//        }
+//    }
+//
+//    private void RED1() {
+//
+//    }
+//
+//    private void RED2() {
+//
+//    }
+//
+//    private void BLUE1() {
+//
+//    }
+//
+//    private void BLUE2() {
+//
+//    }
+//}
+//
+//public class SCENARIOS {
+//    public static final int RED1 = 1;
+//    public static final int RED2 = 2;
+//    public static final int BLUE1 = 3;
+//    public static final int BLUE2 = 4;
+//}
