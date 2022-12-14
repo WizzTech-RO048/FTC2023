0ae86099aa02a17a31932cb88199ddbbc24e11ad
VladTomici14 <vladtomici@gmail.com>
2022-12-14 16:05:31 +0200

    Added a state for each start situation.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomy/States/Scenarios/StatePositionMiddle.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Autonomy.States.Scenarios;
+
+import android.os.Build;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import org.firstinspires.ftc.teamcode.Autonomy.Detector;
+import org.firstinspires.ftc.teamcode.Autonomy.States.*;
+import org.firstinspires.ftc.teamcode.Robot.Robot;
+import org.firstinspires.ftc.teamcode.Robot.Wheels;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class StatePositionMiddle extends State {
+    private boolean hasMovedForward = false;
+    private boolean hasMovedLeft = false;
+
+    public StatePositionMiddle(@NonNull Robot robot) {
+        super(robot);
+    }
+
+    @Override
+    public State update() {
+
+        if (!hasMovedForward) {
+            hasMovedForward = true;
+            return new StateMove(robot, 0.5, 0.3, Wheels.MoveDirection.FORWARD, this, 10);
+        }
+
+        return this;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomy/States/Scenarios/StatePositionRight.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Autonomy.States.Scenarios;
+
+import android.os.Build;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import org.firstinspires.ftc.teamcode.Autonomy.Detector;
+import org.firstinspires.ftc.teamcode.Autonomy.States.*;
+import org.firstinspires.ftc.teamcode.Robot.Robot;
+import org.firstinspires.ftc.teamcode.Robot.Wheels;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class StatePositionRight extends State {
+    private boolean hasMovedForward = false;
+    private boolean hasMovedLeft = false;
+
+    public StatePositionRight(@NonNull Robot robot) {
+        super(robot);
+    }
+
+    @Override
+    public State update() {
+
+        if (!hasMovedForward) {
+            hasMovedForward = true;
+            return new StateMove(robot, 0.5, 0.3, Wheels.MoveDirection.FORWARD, this, 10);
+        }
+
+        if (!hasMovedLeft) {
+            hasMovedLeft = true;
+            return new StateMove(robot, 0.5, 0.3, Wheels.MoveDirection.RIGHT, this, 10);
+        }
+
+        return this;
+    }
+}
