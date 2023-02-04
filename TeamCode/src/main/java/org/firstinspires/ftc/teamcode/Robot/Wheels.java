2e99fc814c91667f0d31a778f303652bbda01b1a
VladTomici14 <vladtomici@gmail.com>
2023-02-05 01:05:20 +0200

    Started the odometry implementation. Started a function that slows down the robot when sudden moves of the joystick are detected.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Wheels.java
@@ -1,33 +1,25 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
 
-import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import kotlin.text.CharDirectionality;
+import kotlin.Triple;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-import com.qualcomm.hardware.*;
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.stream.Stream;
 
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class Wheels {
+    // ---------- engines variables for setting up each engine -------
+    private List<DcMotorEx> engines = new ArrayList<>();
+    public DcMotor lf, lr, rf, rr;
     private static final List<String> MOTORS_NAMES = Arrays.asList(
             "left_front",
             "left_rear",
@@ -35,8 +27,28 @@ public class Wheels {
             "right_rear"
     );
 
-    private List<DcMotorEx> engines = new ArrayList<>();
-    public DcMotor lf, lr, rf, rr;
+    // -------- odometry vairables ----------
+    // public DcMotor encoderLeft, encoderRight, encoderAux;
+    // final static double L = 5.1; // distance between encoder 1 and 2 in cm
+    // final static double B = 15.7; // distance between midpoint of encoder 1 & 2 and encoder 3
+    // final static double R = 3.0; // wheel radius in cm
+    // final static double N = 8192; // encoder ticks per revolution; REV encoder
+    // final static double cm_per_tick = 2.0 * Math.PI, * R / N;
+
+    // -------- variables for keeping track of encoders position
+    private int currentLeftPosition = 0;
+    private int currentRightPosition = 0;
+    private int currentAuxPosition = 0;
+
+    private int previousLeftPosition = 0;
+    private int previousRightPosition = 0;
+    private int previousAuxPosition = 0;
+
+    // ------- 'slow down' before sudden changes of direction variables ---------
+    private int VMAX = 100;
+    public Triple<Double, Double, Double>[] joystickReadings = new Triple[VMAX];
+    private int joystickReadingSize = 3;
+    private double slowDownPercentage = 1;
 
     private final Telemetry telemetry;
     private final ScheduledExecutorService scheduler;
@@ -62,7 +74,60 @@ public class Wheels {
         rf = setEngine(map, true, true, "right_front");
         rr = setEngine(map, true, true, "right_rear");
 
+        // encoderLeft = lr;
+        // encoderRight = rr;
+        // encoderAux = rf;
+
+//        resetDriverEncoders();
     }
+
+    private void resetDriverEncoders() {
+        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        lr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    // public XyhVector START_POS = new XyhVector(213, 102, Math.toRadians(-174));
+    // public XyhVector pos = new XyhVector(START_POS);
+    //
+    //
+    // // TODO: write this class as it should be written / search in on the web
+    // public static class XyhVector {
+    //     public int x;
+    //     public int y;
+    //     public int h;
+    // }
+    //
+    // public void odometry() {
+    //     previousLeftPosition = currentLeftPosition;
+    //     previousRightPosition = currentRightPosition;
+    //     previousAuxPosition = currentAuxPosition;
+    //
+    //     currentLeftPosition = -encoderRight.getCurrentPosition();
+    //     currentLeftPosition = -encoderLeft.getCurrentPosition();
+    //     currentAuxPosition = encoderAux.getCurrentPosition();
+    //
+    //     int dn1 = currentLeftPosition - previousLeftPosition;
+    //     int dn2 = currentRightPosition - previousRightPosition;
+    //     int dn3 = currentAuxPosition - previousAuxPosition;
+    //
+    //     double dtheta = cm_per_tick * (dn2-dn1) / L;
+    //     double dx = cm_per_tick * (dn1+dn2) / 2.0;
+    //     double dy = cm_per_tick * (dn3 - (dn2-dn1) * B / L);
+    //
+    //     double theta = pos.h + (dtheta / 2.0);
+    //     pos.x += dx * Math.cos(theta) - dy * Math.sin(theta);
+    //     pos.y += dx + Math.sin(theta) + dy * Math.cos(theta);
+    //     pos.h += dtheta;
+    // }
 
     private static DcMotor setEngine(HardwareMap map,
                                        boolean useZeroPowerBehaviorBraking,
@@ -83,7 +148,18 @@ public class Wheels {
         DcMotor engine = map.get(DcMotor.class, name);
 
         // --------- setting the direction of the engine ---------
-        engine.setDirection(name.contains("right_front") || name.contains("left_front") ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE);
+        // WARNING: this might not work
+//        engine.setDirection(name.contains("left") || name.contains("right") ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
+        // -------------- THE FIX --------------
+         if (name.contains("left_front")) {
+             engine.setDirection(DcMotorSimple.Direction.FORWARD);
+         } else if (name.contains("left_rear")) {
+             engine.setDirection(DcMotorSimple.Direction.FORWARD);
+         } else if (name.contains("right_front")) {
+             engine.setDirection(DcMotorSimple.Direction.REVERSE);
+         } else if (name.contains("right_rear")) {
+             engine.setDirection(DcMotorSimple.Direction.REVERSE);
+         }
 
         // ------- setting the encoders for the engine ------
         engine.setMode(useEncoders ? DcMotor.RunMode.RUN_USING_ENCODER : DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -94,18 +170,55 @@ public class Wheels {
         return engine;
     }
 
+
+    private double calculateSlowDownPercentage(Triple<Double, Double, Double>[] joystickReadings) {
+        /**
+         * The goal of this function is to increase the robot
+         * */
+        double percentage = 1.0;
+
+        double x_axis_diff = joystickReadings[2].getFirst() - joystickReadings[1].getFirst();
+        double y_axis_diff = joystickReadings[2].getSecond() - joystickReadings[1].getSecond();
+        double r_axis_diff = joystickReadings[2].getThird() - joystickReadings[1].getThird();
+
+        if(x_axis_diff > 1.0 || y_axis_diff > 1.0 || r_axis_diff > 1.0) { percentage = 0.5; }
+        if (x_axis_diff < 0.2 || y_axis_diff < 0.2 || r_axis_diff < 0.2) { percentage = 1.0; }
+
+        return percentage;
+    }
+
     public void move(double x, double y, double rotation, boolean useArcadeMode) {
-        x = Math.pow(x, 3.0);
-        y = Math.pow(y, 3.0);
+
+        // TODO: think about the implementation of this 'slow down' feature
+        // --------- slowing down between sudden changes of direction ----------
+        // Triple<Double, Double, Double> coordinates = new Triple<>(x, y, rotation);
+        // if (values_read >= joystickReadingSize) {
+        //     for(int i = 1; i < joystickReadingSize; i++) {
+        //         joystickReadings[i-1] = joystickReadings[i];
+        //     }
+        //     joystickReadings[joystickReadingSize-1] = coordinates;
+        // } else {
+        //     joystickReadings[values_read] = coordinates;
+        //     values_read++;
+        // }
+        //
+        // if (joystickReadings.length == joystickReadingSize) {
+        //     slowDownPercentage = calculateSlowDownPercentage(joystickReadings);
+        // } else {
+        //     slowDownPercentage = 1;
+        // }
+
+        x = Math.pow(x * slowDownPercentage, 3.0);
+        y = Math.pow(y * slowDownPercentage, 3.0);
         rotation = Math.pow(rotation, 3.0);
 
         final double direction = Math.atan2(x, y) + (useArcadeMode ? 0.0 : imu_sensor.getHeading());
         final double speed = Math.min(1.0, Math.sqrt(x * x + y * y));
 
-        final double lf = speed * Math.sin(direction + Math.PI / 4.0) + rotation;
-        final double rf = speed * Math.cos(direction + Math.PI / 4.0) - rotation;
-        final double lr = speed * Math.cos(direction + Math.PI / 4.0) + rotation;
-        final double rr = speed * Math.sin(direction + Math.PI / 4.0) - rotation;
+        final double lf = speed * Math.cos(direction + Math.PI / 4.0) + rotation;
+        final double rf = speed * Math.sin(direction + Math.PI / 4.0) - rotation;
+        final double lr = speed * Math.sin(direction + Math.PI / 4.0) + rotation;
+        final double rr = speed * Math.cos(direction + Math.PI / 4.0) - rotation;
 
         setMotors(lf, lr, rf, rr);
     }
